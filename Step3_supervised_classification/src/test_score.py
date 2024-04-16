3b888dc40da48c630b079944335571800a2beb97
JulieLascar <julascar@yahoo.fr>
2024-04-16 20:00:18 +0200

    Update test_score.py

--- Step3_supervised_classification/src/test_score.py
+++ Step3_supervised_classification/src/test_score.py
@@ -3,30 +3,32 @@ import torch
 import pickle
 from torch.utils.data import DataLoader
 from datasets import Videofeatures_Labels_Dataset
+import os
 
 """
 Print scores for the test dataset.
 """
 
 # Args :
-dataset_name = "Mediapi_Expert/"
+dataset_name = "Mediapi_Expert"
 model_name = "MLP2_2"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Computes scores on test set :
 dataset_path = "Step3_supervised_annotation/" + dataset_name + "/"
 
-savepath = Path(f"{dataset_name}models/{model_name}.pch")
+savepath = Path(os.path.join(dataset_name, "models", model_name + ".pch"))
 with savepath.open("rb") as fp:
     state = torch.load(fp)
 model = state.model
 model.to(device)
 model.eval()
 
-d_Vid2Labels = pickle.load(open(dataset_path + "saved_files/d_Vid2Labels.pkl", "rb"))
-d_Gid2gloses = pickle.load(open(dataset_path + "saved_files/d_Gid2gloses.pkl", "rb"))
+save_files_path = os.path.join(dataset_path, "saved_files")
+d_Vid2Labels = pickle.load(open(os.path.join(save_files_path, "d_Vid2Labels.pkl"), "rb"))
+d_Gid2gloses = pickle.load(open(os.path.join(save_files_path, "d_Gid2gloses.pkl"), "rb"))
 
-DTest = pickle.load(open(dataset_path + "saved_files/DTest.pkl", "rb"))
+DTest = pickle.load(open(os.path.join(save_files_path, "DTest.pkl"), "rb"))
 Test_dataset = Videofeatures_Labels_Dataset(d_Vid2Labels, DTest)
 Test_dataloader = DataLoader(Test_dataset, batch_size=1, shuffle=False)
 
